1f966eaa8a56ddf2d667f638baa5b890c4b19ab9
Maks Nemisj <maks@nemisj.com>
2017-05-03 21:52:59 +0200

    Added routing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,50 +1,107 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
 
 import styles from './app.sass';
 
 // components
 import Button from './button.jsx';
 
-export default () => {
-  return (
-    <div className={styles.fullHeght}>
-      <header className={`container-fluid ${styles.headerContainer}`}>
-        <div className={`row ${styles.fullHeght}`}>
-          <div className={`col ${styles.logoContainer} align-self-center ${styles.fullHeght}`}>
-            <span className={styles.textStart}>start</span><span className={styles.textMail}>mail</span>
-          </div>
-          <div className={`col align-self-center ${styles.fullHeght}`}>
-            <h3 style={{display: 'inline-block'}} className={styles.title}>Inbox</h3>
-            <Button label="Compose" mode="blue" />
-            <Button label="Refresh" />
-          </div>
-        </div>
-      </header>
-      <div className={styles.autoHeightContainer}>
-        <div className={`container-fluid ${styles.fullHeght}`}>
+class Inbox extends React.Component {
+  renderEmailItem() {
+    if (this.props.emailId) {
+      return <div>{this.props.emailId}</div>
+    }
+    return null;
+  }
+
+  render() {
+    return (
+      <div>
+        Inbox content
+        {this.renderEmailItem()}
+      </div>
+    );
+  }
+};
+
+const Drafts = (props) => <div>Draft content</div>;
+const Sent = (props) => <div>Sent content</div>;
+const Trash = (props) => <div>Trash content</div>;
+const Zork = (props) => <div>Zork content</div>;
+
+class App extends React.Component {
+  renderNavBar() {
+    return (
+      <navbar className={`col ${styles.navbarContiner}`}>
+        <ul className={styles.menu}>
+          <li><NavLink className={styles.menuItem} activeClassName={styles.menuItemActive} to="/inbox">Inbox</NavLink></li>
+          <li><NavLink className={styles.menuItem} activeClassName={styles.menuItemActive} to="/drafts">Drafts</NavLink></li>
+          <li><NavLink className={styles.menuItem} activeClassName={styles.menuItemActive} to="/sent">Sent</NavLink></li>
+          <li><NavLink className={styles.menuItem} activeClassName={styles.menuItemActive} to="/trash">Trash</NavLink></li>
+          <li><NavLink className={styles.menuItem} activeClassName={styles.menuItemActive} to="/spam">Spam</NavLink></li>
+        </ul>
+        <div className={styles.devider}/>
+        <ul>
+          <li className={styles.menuItem}>Contacts</li>
+          <li className={styles.menuItem}>Settings</li>
+          <li className={styles.menuItem}>Support</li>
+        </ul>
+      </navbar>
+    );
+  }
+
+  renderMainContent() {
+    return (
+      <div className={`col ${styles.mainContainer}`}>
+        <Route exact path="/" component={Inbox} />
+        <Switch>
+          <Route exact path="/inbox/view/:emailId" render={route => (
+            <Inbox emailId={route.match.params.emailId}/>
+          )}/>
+          <Route path="/inbox/" component={Inbox} />
+        </Switch>
+
+        <Route path="/drafts" component={Drafts} />
+        <Route path="/sent" component={Sent} />
+        <Route path="/trash" component={Trash} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={styles.fullHeght}>
+        <header className={`container-fluid ${styles.headerContainer}`}>
           <div className={`row ${styles.fullHeght}`}>
-            <navbar className={`col ${styles.navbarContiner}`}>
-              <ul className={styles.menu}>
-                <li className={`${styles.menuItem} ${styles.menuItemActive}`}>Inbox</li>
-                <li className={styles.menuItem}>Drafts</li>
-                <li className={styles.menuItem}>Sent</li>
-                <li className={styles.menuItem}>Trash</li>
-                <li className={styles.menuItem}>Spam</li>
-              </ul>
-              <div className={styles.devider}/>
-              <ul>
-                <li className={styles.menuItem}>Contacts</li>
-                <li className={styles.menuItem}>Settings</li>
-                <li className={styles.menuItem}>Support</li>
-              </ul>
-            </navbar>
-            <div className={`col ${styles.mainContainer}`}>
-              Main content
+            <div className={`col ${styles.logoContainer} align-self-center ${styles.fullHeght}`}>
+              <span className={styles.textStart}>start</span><span className={styles.textMail}>mail</span>
             </div>
+            <div className={`col align-self-center ${styles.fullHeght}`}>
+              <h3 style={{display: 'inline-block'}} className={styles.title}>Inbox</h3>
+              <Button label="Compose" mode="blue" />
+              <Button label="Refresh" />
+            </div>
+          </div>
+        </header>
+        <div className={styles.autoHeightContainer}>
+          <div className={`container-fluid ${styles.fullHeght}`}>
+            <Router>
+              <div className={`row ${styles.fullHeght}`}>
+                {this.renderNavBar()}
+                {this.renderMainContent()}
+              </div>
+            </Router>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default App;
